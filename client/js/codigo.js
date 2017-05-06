380b74b579062232e622ae5ed5b54a6b5ab75ed7
Salva79 <salvador.e.medina@gmail.com>
2017-05-06 14:56:03 +0200

    prueba ajax1

--- client/js/codigo.js
+++ client/js/codigo.js
@@ -1,9 +1,36 @@
+function conexion(){
+	var envio = {
+		username: 'nombre',
+		password: 'password'
+	}
+	$.ajax({
+		async: true,
+		dataType: 'json',
+		data: envio,
+		method: 'Post',
+		url: '/api/Usuarios/login',
+		error: function (xhr){
+			console.log('Error ' + xhr.statusText + ' envio de datos');
+		},
+		sucess:function (respuesta){
+			console.log('Enviado, reciviendo:');
+			console.log(respuesta);
+		},
+		complete:function () {
+			console.log('Enviado terminado');
+		}
+	});
+}
 $(document).ready(function() {
+	console.log('cargado');
+	$('#enviar').click(function() {
+		console.log('pulsado');
+		conexion();
+	});	
 	$('#botonPerfil').click(function() {
 		window.location="perfil.html";
 	});
-
 	$('#botonVolver').click(function() {
 		window.history.back();
 	});
-});
+})
