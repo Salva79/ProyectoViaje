b95563fb153959d763065d204c7b8a5f638675e6
Salva79 <salvador.e.medina@gmail.com>
2017-05-06 14:47:58 +0200

    ajax creado logueo alumno

--- client/js/codigo.js
+++ client/js/codigo.js
@@ -1,31 +1,39 @@
-function conexion(){
-	var envio = {
-		username: 'nombre',
-		password: 'password'
-	}
+function conexion(datos,url){
 	$.ajax({
 		async: true,
 		dataType: 'json',
-		data: envio,
-		method: 'Post',
-		url: '/api/Usuarios/login',
-		error: function (xhr){
-			console.log('Error ' + xhr.statusText + ' envio de datos');
-		},
-		sucess:function (respuesta){
-			console.log('Enviado, reciviendo:');
-			console.log(respuesta);
-		},
-		complete:function () {
-			console.log('Enviado terminado');
-		}
-	});
+		data: datos,
+		method: 'POST',
+		url: url,
+	}).done(function (respuesta){
+			if(typeof(respuesta.id) !== undefined){
+				/*Guardamos los datos del usuario para acceder desde otras html
+				sin pasar por url, siempre se pueden codificar los datos*/
+				sessionStorage.userId=respuesta.userId;
+				sessionStorage.userToken=respuesta.id;
+				sessionStorage.userTtl=respuesta.ttl;
+				sessionStorage.userCreated=respuesta.created;
+				window.location.href = "alumnos/inicio.html";
+			}else{
+				alert("No exite el usuario");
+			}
+	}).fail(function (xhr){
+			if(xhr.statusText === 'Unauthorized'){
+				alert('Error, usuario no registrado');	
+			}else{
+				alert('Error en el envio de datos');
+			}			
+	});		
 }
+
 $(document).ready(function() {
-	console.log('cargado');
-	$('#enviar').click(function() {
-		console.log('pulsado');
-		conexion();
+	$('#enviar').click(function(e) {
+		var envio = {
+			"username": $("#usuario").val(),
+			"password": $("#password").val()
+		}
+		var destino = '/api/Usuarios/login'
+		conexion(envio,destino);
 	});	
 	$('#botonPerfil').click(function() {
 		window.location="perfil.html";
