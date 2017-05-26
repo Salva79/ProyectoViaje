38d1e63ea2a6ab28be51b7aa22a50200549b4d3a
Salva79 <salvador.e.medina@gmail.com>
2017-05-27 00:49:18 +0200

    alta listado borrado categorias

---
+++ client/js/altaCategorias.js
@@ -0,0 +1,91 @@
+function estilosinfo() {
+	$('#info').removeClass();
+	$('#info').addClass('alert alert-success');
+}
+function eliminarinfo() {
+	setTimeout(function(){
+        $('#info').html("");
+        $('#info').removeClass('alert alert-success');}, 2500);
+}
+
+/* Eliminar los valores de sesión */
+function eliminarStorage(){ 
+	sessionStorage.removeItem("userToken");
+	sessionStorage.removeItem("Nombre"); 
+}
+
+/* Vaciar los campos, después de seleccionar el botón enviar */
+function reiniciarElementos() {
+	$("#nombre").val("");
+}
+
+function estilosAlerta() {
+	$('#info').removeClass();
+	$('#info').addClass('alert alert-danger');
+}
+
+/* Eliminar la alerta de información */
+function eliminarAlerta() {
+	setTimeout(function() {
+		$('#info').html("");
+		$('#info').removeClass();
+	}, 2500);
+}
+
+/* Función para insertar proveedores */
+function insertarCategoria(datos,url) {
+	$.ajax({
+		async: true,
+		dataType: 'json',
+		data: datos,
+		method: 'POST',
+		url: url,
+	}).done(function(respuesta) {
+		if (typeof(respuesta.id) !== undefined) {
+			estilosinfo();
+			$('#info').html("Categoria insertada");
+			eliminarinfo();
+			window.location.href = "../inicio.html";
+			
+		} else {
+			estilosAlerta();
+			$('#info').html("Error, categoria no insertada");
+			eliminarAlerta();
+		}
+	});
+}
+
+/* Función para comprobar los datos introducidos */
+function validarDatos() {
+	var nombre = $("#nombre").val();
+	nombre = nombre.trim();
+	if (nombre == "") {
+		$('#info').html("El nombre es obligatorio");
+		$('#info').addClass('alert alert-danger');
+	} else {
+		var datosEnvio = {
+			"Nombre": nombre
+		}
+		var destino = '/api/TipoProductos?access_token=' + sessionStorage.userToken;
+		insertarCategoria(datosEnvio, destino);
+	}
+		reiniciarElementos();
+		eliminarAlerta();
+}
+
+$(document).ready(function() {
+	var nombre = "<i class='fa fa-user-circle' aria-hidden='true'></i> " + sessionStorage.userNombre;
+	$("#botonPerfil").html(nombre);
+	$("#botonSalir").click(function(){
+		eliminarStorage();
+		window.location.href = "../../index.html";
+	});
+
+	$("#botonPerfil").click(function(){
+		window.location.href = "../../perfil.html";
+	});
+
+	$('#insertar').click(function() {
+		validarDatos();
+	});
+})
---
+++ client/js/listadoCategorias.js
@@ -0,0 +1,108 @@
+var direccionCategorias = '/api/TipoProductos?access_token=' + sessionStorage.userToken;
+/* Eliminar los valores de sesión */
+function eliminarStorage(){ 
+	sessionStorage.removeItem("userToken");
+	sessionStorage.removeItem("Nombre"); 
+}
+
+/* Vaciar los campos, después de seleccionar el botón enviar */
+function reiniciarElementos() {
+	$("#nombre").val("");
+}
+
+/* Eliminar la alerta de información */
+function eliminarAlerta() {
+	setTimeout(function() {
+		$('#info').html("");
+		$('#info').removeClass();
+	}, 2500);
+}
+
+function borraProveedor(id){
+	var url = '/api/TipoProductos/' + id + '?access_token=' + sessionStorage.userToken;
+	$.ajax({
+		async: true,
+		dataType: 'json',
+		data: "",
+		method: 'DELETE',
+		url: url,
+	}).done(function (respuesta){
+		if(respuesta.count === 1){
+			window.location.href = "listadoCategorias.html";	
+		}else{
+			estilosAlerta();
+			$('#info').html("Error, categoria no borrada");
+			console.log("Error, proveedor no borrado");
+			eliminarAlerta();
+		}
+	}).fail(function (xhr){
+			if(xhr.statusText === 'Unauthorized'){
+				estilosAlerta();
+				$('#info').html("Error, usuario no registrado");
+				console.log("Error, usuario no registrado");
+				eliminarAlerta();	
+			}else{
+				estilosAlerta();
+				$('#info').html("Error en el envio de datos");
+				console.log("Error en el envio de datos");
+				eliminarAlerta();
+			}
+			eliminarStorage();
+			window.location.href = "../index.html";			
+	});
+}
+
+function conexion(metodo,datos,url){
+	$.ajax({
+		async: true,
+		dataType: 'json',
+		data: datos,
+		method: metodo,
+		url: url,
+	}).done(function (respuesta){
+			if(typeof(respuesta) !== undefined){
+				var cadena = "<div class='listado'>";
+				if(respuesta.length>0){
+					for(var i = 0; i < respuesta.length; i++){
+						cadena = cadena + (i+1) + " -   Nombre: " + respuesta[i].Nombre + " <button type='button' id='borrar' onclick='borraProveedor(" + respuesta[i].id + ")' class='btn btn-danger botonForm btn-xs'>BORRAR</button><br>";
+					}
+					cadena = cadena + "</div>";
+					$('#contienelistados').html(cadena);
+				}else{
+					$('#contienelistados').html("No hay categoria");
+				}
+			}else{
+				estilosAlerta();
+				$('#info').html("No exite el usuario");
+				console.log("No exite el usuario");
+				nombre = "<i class='fa fa-user-circle' aria-hidden='true'></i> --- ";
+				eliminarAlerta();
+			}
+	}).fail(function (xhr){
+			if(xhr.statusText === 'Unauthorized'){
+				estilosAlerta();
+				$('#info').html("Error, usuario no registrado");
+				console.log("Error, usuario no registrado");
+				eliminarAlerta();	
+			}else{
+				estilosAlerta();
+				$('#info').html("Error en el envio de datos");
+				console.log("Error en el envio de datos");
+				eliminarAlerta();
+			}
+			eliminarStorage();
+			window.location.href = "../index.html";			
+	});		
+}
+
+$(document).ready(function() {
+	conexion("GET","",direccionCategorias)
+	$("#botonPerfil").html(("<i class='fa fa-user-circle' aria-hidden='true'></i> " + sessionStorage.userNombre));
+	$("#botonSalir").click(function(){
+		eliminarStorage();
+		window.location.href = "../../index.html";
+	});
+	$("#botonPerfil").click(function(){
+		window.location.href = "../perfil.html";
+	});
+})
