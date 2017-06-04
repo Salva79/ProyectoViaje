117a5bd051c58108570fa7920de46f967044fc08
Salva79 <salvador.e.medina@gmail.com>
2017-06-04 13:47:54 +0200

    ingresos coordinador sin terminar

---
+++ client/js/ingresosSinVerificar.js
@@ -0,0 +1,59 @@
+/* Eliminar los valores de sesión */
+function eliminarStorage(){ 
+	sessionStorage.removeItem("userToken");
+	sessionStorage.removeItem("userId");
+	sessionStorage.removeItem("userTtl");
+	sessionStorage.removeItem("userCreated");
+	sessionStorage.removeItem("userNombre");
+	sessionStorage.removeItem("userApellidos");
+	sessionStorage.removeItem("userDni");
+	sessionStorage.removeItem("userTelefono");
+	sessionStorage.removeItem("userCurso");
+	sessionStorage.removeItem("userUsername");
+	sessionStorage.removeItem("userEmail");
+	sessionStorage.removeItem("userPassword");
+	sessionStorage.removeItem("userObjetivoId");
+	sessionStorage.removeItem("userCentroId"); 
+	sessionStorage.removeItem("NombreCentro"); 
+	sessionStorage.removeItem("CodigoCentro");
+	sessionStorage.removeItem("LocalidadCentro");
+	sessionStorage.removeItem("userIdAlumnado");
+	sessionStorage.removeItem("NombreObjetivo");     
+}
+
+function estilosinfo() {
+	$('#info').removeClass();
+	$('#info').addClass('alert alert-success');
+}
+function eliminarinfo() {
+	setTimeout(function(){
+        $('#info').html("");
+        $('#info').removeClass('alert alert-success');}, 2500);
+}
+function estilosAlerta() {
+	$('#info').removeClass();
+	$('#info').addClass('alert alert-danger');
+}
+function eliminarAlerta() {
+	setTimeout(function(){
+        $('#info').html("");
+        $('#info').removeClass('alert alert-danger');}, 2500);
+}
+
+
+$(document).ready(function() {
+	var nombre = "<i class='fa fa-user-circle' aria-hidden='true'></i> " + sessionStorage.userNombre;
+	$("#botonPerfil").html(nombre);
+	$("#botonSalir").click(function(){
+		eliminarStorage();
+		window.location.href = "../../index.html";
+	});
+	$("#botonPerfil").click(function(){
+		window.location.href = "../perfil.html";
+	});
+})
+
+
+
+
+
