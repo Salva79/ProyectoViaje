a37ddaf5518f911678b2f405daba7f9290c19a9e
Alumno Servidor <you@example.com>
2017-06-17 18:14:17 +0200

    listado pedidos alumno corrección

--- client/js/alumno/listadoPedidos.js
+++ client/js/alumno/listadoPedidos.js
@@ -87,14 +87,13 @@ function cargarDatos(){
 	var nombre = ("<i class='fa fa-user-circle' aria-hidden='true'></i> " + sessionStorage.userNombre);
 	$("#botonPerfil").html(nombre);
 	var cadena = "";
-	if(arrayPedidos>0){
+	if(arrayPedidos.length > 0){
 		for(var i=0 ; i<arrayPedidos.length ; i++){
 			var anyo = arrayFechas[i].substring(0,4);
 			var mes = arrayFechas[i].substring(5,7);
 			var dia = arrayFechas[i].substring(8,10);
 			cadena = cadena + (i+1) + " - Fecha de Pedido: " + dia + " - " + mes + " - " + anyo + "<br>       Cantidad: " + arrayDetalles[i][0] + "€    -    Entregado: " + arrayDetalles[i][1] + "€<br>";
 		}
-		cadena = cadena;
 	}else{
 		cadena = "No dispones de pedidos realizados";
 	}
